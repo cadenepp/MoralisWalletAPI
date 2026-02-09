f3bda88e8fb0bc130502cb81603de03ca013bf1c
Arun <arun_graeff@stu.indianhills.edu>
2026-02-09 11:46:26 -0600

    repos

--- WalletAPI.Infrastructure/Repositories/UserRepository.cs
+++ WalletAPI.Infrastructure/Repositories/UserRepository.cs
@@ -16,7 +16,7 @@ public class UserRepository : Repository<User>, IUserRepository
 
     public async Task DeleteAsync(int id, CancellationToken cancellationToken = default)
     {
-        var user = await _context.Users.FindAsync(id);
+        var user = await _context.Users.FindAsync(id ,cancellationToken);
         _context.Users.Remove(user);
         await _context.SaveChangesAsync(cancellationToken);
     }
