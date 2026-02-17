546e867ae74667b1bf72927c2e63cf6f55d5a842
Arun <arun_graeff@stu.indianhills.edu>
2026-02-17 10:23:47 -0600

    userService

--- WalletAPI.Application/Services/CreateUserService.cs
+++ WalletAPI.Application/Services/CreateUserService.cs
@@ -1,6 +1,43 @@
+using WalletAPI.Domain.Interfaces;
+using WalletAPI.Domain.Models;
 namespace WalletAPI.Application.Services;
 
 public class CreateUserService
 {
+    private readonly UserRepository _userRepository;
+    public CreateUserService(UserRepository userRepository)
+    {
+        _userRepository = userRepository;
+        
+    }
+    public async Task CreateAsync(string username, string password, int age, string email, string reason,
+        CancellationToken cancellationToken = default)
+    {
+        var user = new User
+        {
+            Username = username,
+            Password = password,
+            Age = age,
+            Email = email,
+            Reason = reason,
+        };
+        await _userRepository.AddAsync(user, cancellationToken);
+
+        await _userRepository.SaveChangesAsync(); 
+    }
+    
+    public async Task DeleteAsync( int userid, CancellationToken cancellationToken = default)
+    {
+
+        var userServices  = await _userRepository.GetByIdAsync(userid, cancellationToken);
+        await _userRepository.SaveChangesAsync();
+    }
+
+    public async Task<object> GetUserByIdAsync( int userid, CancellationToken cancellationToken = default)
+    {
+        var user = await _userRepository.GetByIdAsync(userid, cancellationToken);
+        return user;
+    }
+        
     // TODO: Crud Logic for Creating a User - Arun
 }
---
+++ WalletAPI.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,21 @@
+using WalletAPI.Domain.Models;
+
+namespace WalletAPI.Domain.Interfaces;
+
+public interface UserRepository 
+{
+    public async Task AddAsync(User user, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<object> GetByIdAsync(int userid, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task SaveChangesAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
